93b87e5c9b04fa175147eb8cd65746abfbaa1225
nitesh-20 <nsahu6156@gmail.com>
2024-02-29 23:00:05 +0530

    Landingpage-fix

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,17 +3,24 @@ import { FaArrowUpLong } from "react-icons/fa6";
 const LandingPage = () => {
   return (
     <div className='w-full h-screen bg-zinc-900 pt-[0.1px]'>
-            <div className="textStructre mt-52 px-20">
+            <div className="textStructre mt-40 px-20">
                 {["We Create","Eye Opening","Presentation"].map((item,index) => {
-                    return     <div className="masker"> 
-                    <h1 className="uppercase text-[7.5vw] leading-[6vw]  font-['Founders_Grotesk X-Condensed'] font-bold">
+                    return (   
+                     <div className="masker"> 
+                       <div className="w-fit flex">
+                        {index === 1 && (
+                        <div className="mr-3 rounded-md w-[9vw] h-[11vh]  relative -top-[-1.1vw] bg-red-500"></div>
+                        )}
+                        <h1 className="flex items-center uppercase text-[7.8vw] leading-[7vw]  font-['Founders_Grotesk X-Condensed'] font-bold">
                         {item}
                     </h1>
+                  </div>
                 </div>
-                })}
-            
+             );   
+            })}
+
             </div>
-            <div className="border-t-[1px] border-zinc-800 mt-[55px] flex justify-between items-center py-4 px-20">
+            <div className="border-t-[1px] border-zinc-800 mt-[120px] flex justify-between items-center py-4 px-20">
                 {["For public and private companies","From the first pitch to IPO"].map((item,index) => (
                     <p className="text-md font-light tyracking-tight leading-none">{item}</p>
                 ))}
