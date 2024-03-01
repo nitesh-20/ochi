de199055a59bf5ea898e291c36b6efdf07778ae5
nitesh-20 <nsahu6156@gmail.com>
2024-03-01 23:21:28 +0530

    Marque

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,9 +1,13 @@
+import { motion } from 'framer-motion'
 import React from 'react'
 
 const Marquee = () => {
   return (
-    <div className='w-full h-[50vh] bg-green-900'>
-      
+    <div className='w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]'>
+      <div className="text border-t-2 border-b-2 border-zinc-400 flex gap-10 overflow-hidden whitespace-nowrap">
+        <motion.h1 initial={{x: 0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat:Infinity, duration:5}} className="text-[24vw] leading-none font-['Founders_Grotesk X-Condensed']  uppercase pt-1 mb-5 font-semibold">we are ochi</motion.h1>
+        <motion.h1 initial={{x: 0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat:Infinity, duration:5}} className="text-[24vw] leading-none font-['Founders_Grotesk X-Condensed']  uppercase pt-1 mb-5 font-semibold" >we are ochi</motion.h1>
+      </div>
     </div>
   )
 }
